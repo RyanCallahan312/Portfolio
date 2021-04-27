e2e87745288eb3495642927e644d5da4b5b554ac
Ryan Callahan <RyanCallahan312@gmail.com>
2021-04-27 17:46:21 -0400

    Update projectCard.tsx

--- src/components/projectCard/projectCard.tsx
+++ src/components/projectCard/projectCard.tsx
@@ -1,118 +1,35 @@
 import * as React from 'react';
-import { GatsbyImage, getImage, IGatsbyImageData } from 'gatsby-plugin-image';
 import { useStaticQuery, graphql } from 'gatsby';
-import { IProjectInfo, projectMappings } from '@constants/projectMappings';
-import { useMediaQuery } from 'react-responsive';
-
-import '@styles/projectCard.css';
 
 interface Icons {
 	allFile: {
-		nodes: [{ publicURL: string; name: string }];
+		nodes: [{ publicURL: string}];
 	};
 }
 
-interface Props {
-	image: IGatsbyImageData;
-	base: string;
-}
-
-const ProjectCard: React.FC<Props> = ({ image, base }: Props) => {
-	let projectInfo: IProjectInfo = projectMappings.find(
-		(e) => e.base === base,
-	);
-
-	const isDesktopOrLaptop = useMediaQuery({ minDeviceWidth: 1424 });
-
+const ProjectCard: React.FC = () => {
 	const icons: Icons = useStaticQuery(graphql`
 	{
 		allFile(
-			filter: {relativeDirectory: {eq: "icons"}, name: {in: ["eye-white", "git-white", "no-eye-white"]}}
+			filter: {relativeDirectory: {eq: "icons"}, name: {eq: "eye-white"}}
 		) {
 			nodes {
 				publicURL
-				name
 			}
 		}
 	}
 	`);
 
 	return (
-		<div
-			className='card-container'
-			style={
-				projectInfo.isPronounced && isDesktopOrLaptop
-					? {
-							transform: 'scale(1.1)',
-							margin: '20px',
-					  }
-					: {}
-			}>
-			<div className='card-image'>
-				<GatsbyImage
-					key={projectInfo.description}
-					image={getImage(image)}
-					alt={projectInfo.description}
-					style={{
-						width: '100%',
-						height: '100%',
-						borderRadius: '9px 9px 0px 0px',
-					}}
+		<div>
+			<a>
+				<img
+					width='32px'
+					src={
+						icons.allFile.nodes[0].publicURL
+					}
 				/>
-			</div>
-			<div className='card-text'>
-				<div className='card-title-with-tags'>
-					<h1 className='card-title'>{projectInfo.name}</h1>
-					<h1 className='card-tags'>
-						{projectInfo.displayTags.join(',')}
-					</h1>
-				</div>
-				<div className='card-description-container'>
-					<p className='card-description'>
-						{projectInfo.description}
-					</p>
-				</div>
-			</div>
-			<div className='demo-github-links'>
-				<a
-					className='demo-link'
-					href={projectInfo.github}
-					target='_blank'>
-					<img
-						width='32px'
-						src={
-							icons.allFile.nodes.find(
-								(element) => element.name === 'git-white',
-							).publicURL
-						}
-						alt='github repo link'
-					/>
-				</a>
-				<a
-					className='demo-link'
-					href={projectInfo.demo}
-					target='_blank'>
-					<img
-						width='32px'
-						src={
-							icons.allFile.nodes.find((element) =>
-								projectInfo.demo
-									? element.name === 'eye-white'
-									: element.name === 'no-eye-white',
-							).publicURL
-						}
-						style={
-							projectInfo.demo
-								? {}
-								: {
-										cursor: 'default',
-										opacity: 0.5,
-								  }
-						}
-						alt='project demo link'
-					/>
-				</a>
-			</div>
+			</a>
 		</div>
 	);
 };
