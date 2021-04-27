92931d1d0430ce52f6d37959b59c377501d81547
Ryan Callahan <RyanCallahan312@gmail.com>
2021-04-27 18:22:06 -0400

    minimal bug reproduction

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,57 +1,10 @@
 import * as React from 'react';
-import { useStaticQuery, graphql } from 'gatsby';
-import { useMediaQuery } from 'react-responsive';
 
-import Layout from '@components/common/defaultLayout';
-import SEO from '@components/common/seo';
-import SkillCardRadial from '@components/skillCard/skillCardRadial';
-import SkillCardCarosel from '@components/skillCard/skillCardCarosel';
-import '@styles/index.css';
+import ProjectCard from '@components/projectCard/projectCard';
 
-interface CardUrlQuery {
-	allFile: {
-		nodes: [
-			{
-				publicURL: string;
-			},
-		];
-	};
-}
-
-const IndexPage: React.FC = () => {
-	const data: CardUrlQuery = useStaticQuery(graphql`
-	{
-		allFile(filter: {relativeDirectory: {eq: "skillCards"}}, sort: {fields: name}) {
-			nodes {
-				publicURL
-			}
-		}
-	}
-	`);
-
-	const cardPaths = data.allFile.nodes.map((node) => node.publicURL);
-
-	const isDesktopOrLaptop = useMediaQuery({ minDeviceWidth: 1224 });
-
-	return (
-		<Layout>
-			<SEO title='Home' />
-			<div className='headline-grid-container'>
-				<div className='headline-text-container'>
-					<h1 className='index-header'>👋 I’m Ryan</h1>
-					<h3 className='index-header'>a full stack web developer</h3>
-					<h3 className='index-header'>
-						with a lot of cards in my hand
-					</h3>
-				</div>
-				{isDesktopOrLaptop ? (
-					<SkillCardRadial cardPaths={cardPaths} />
-				) : (
-					<SkillCardCarosel cardPaths={cardPaths} />
-				)}
-			</div>
-		</Layout>
+const Projects: React.FC = () => {
+	return (<p>index page</p>
 	);
 };
 
-export default IndexPage;
+export default Projects;
--- src/pages/projects.tsx
+++ src/pages/projects.tsx
@@ -1,22 +1,9 @@
 import * as React from 'react';
-import { useStaticQuery, graphql } from 'gatsby';
 
-import Layout from '@components/common/defaultLayout';
-import SEO from '@components/common/seo';
-import TopProjects from '@components/projectsPage/topProjects';
-import '@styles/projects.css';
+import ProjectCard from '@components/projectCard/projectCard';
 
 const Projects: React.FC = () => {
-	return (
-		<Layout>
-			<SEO title='Projects' />
-			<div className='headline-grid-container'>
-				<div className='headline-text-container'>
-					<h1 className='projects-header'>Featured Projects</h1>
-				</div>
-				<TopProjects />
-			</div>
-		</Layout>
+	return (<ProjectCard />
 	);
 };
 
